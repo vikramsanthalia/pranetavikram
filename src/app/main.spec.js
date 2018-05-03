d39fdaaaaf96174c2cccc949b9f6ec469692a2ae
Vikram Santhalia <vikram.san@PW-C02VN0ZTG8WN.local>
2018-05-04 02:51:24 +0530

    Initial change

--- src/app/main.spec.js
+++ src/app/main.spec.js
@@ -10,12 +10,8 @@ describe('main component', () => {
     angular.mock.module('app');
   });
 
-  it('should render the header, title, techs and footer', angular.mock.inject(($rootScope, $compile) => {
+  it('should render', angular.mock.inject(($rootScope, $compile) => {
     const element = $compile('<app>Loading...</app>')($rootScope);
     $rootScope.$digest();
-    expect(element.find('fountain-header').length).toEqual(1);
-    expect(element.find('fountain-title').length).toEqual(1);
-    expect(element.find('fountain-techs').length).toEqual(1);
-    expect(element.find('fountain-footer').length).toEqual(1);
   }));
 });
