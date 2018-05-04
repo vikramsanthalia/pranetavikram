d8394c3f666dc65279eddb1101963bd242880b8e
Vikram Santhalia <vikram.san@PW-C02VN0ZTG8WN.local>
2018-05-04 22:53:08 +0530

    Intro

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -71,8 +71,44 @@ function my_proxy (func, obj) {
 
 
   document.addEventListener('DOMContentLoaded', function(event) {
-    HeartsBackground.initialize();
+    //HeartsBackground.initialize();
+    Timer.init();
   });
+
+  var Timer = {
+
+    compute: function(){
+        var date1 = new Date();
+        var date2 = new Date(2018, 5, 21, 19);
+        // var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+        // var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+        var delta = Math.abs(date2 - date1) / 1000;
+        var diffDays = Math.floor(delta / 86400);
+        delta -= diffDays * 86400;
+        var diffhours = Math.floor(delta / 3600) % 24;
+        delta -= diffhours * 3600;
+        var diffminutes = Math.floor(delta / 60) % 60;
+        delta -= diffminutes * 60;
+        var diffseconds = Math.floor(delta % 60);  
+
+        var days = document.querySelector("#days");
+        var hours = document.querySelector("#hours");
+        var mins = document.querySelector("#mins");
+        var secs = document.querySelector("#secs");
+
+        days.innerHTML = diffDays;
+        hours.innerHTML = diffhours;
+        mins.innerHTML = diffminutes;
+        secs.innerHTML = diffseconds;
+    },
+
+    init: function(){
+        var that = this;
+        setInterval(function(){
+            that.compute();
+        },1000);
+    }
+  };
   
 
 (function() {
