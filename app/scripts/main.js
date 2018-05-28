2ef7bfd40ffbf22b5ac65d023faeba9996973f08
Vikram Santhalia <vikram.san@PW-C02VN0ZTG8WN.local>
2018-05-28 11:05:38 +0530

    Added flicks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,6 +3,7 @@ import timer from './timer';
 import map from './mapool';
 import guest from './guest';
 import aos from './aos';
+import Flickity from './flick/index';
 //import bubbles from './bubbles';
 
 (function() {
@@ -17,5 +18,18 @@ import aos from './aos';
     heart().HeartsBackground.initialize();
     timer().Timer.init();
     //bubbles();
+
+    var elem = document.querySelector('.carousel');
+    var flkty = new Flickity(elem, {
+      cellAlign: 'left',
+      contain: true,
+      adaptiveHeight: false,
+      cellAlign: 'right',
+      contain: true,
+      groupCells: '80%',
+      wrapAround: true,
+      freeScroll: true,
+      pageDots: false
+    });
   });
 })();
